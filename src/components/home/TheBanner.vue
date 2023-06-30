f5201957ee208b4f18dbbac4a116153e9cd4ddf8
Shahadat Hossain <33016130+sdtHossain@users.noreply.github.com>
2023-07-01 00:50:01 +0600

    added banner section

--- src/components/home/TheBanner.vue
+++ src/components/home/TheBanner.vue
@@ -7,16 +7,6 @@ import DropdownMenu from '@/components/dropdownMenu/DropdownMenu.vue'
   <div class="bg-gray home-banner">
     <div class="container" style="min-height: 600px">
       <DropdownMenu />
-      <div class="row">
-        <div class="col-lg-6">
-          <h1 class="display-5 fw-bold py-5">Responsive left-aligned hero with image</h1>
-
-          <div class="bg-gray">
-            <TheRating />
-            <span>Based on Customer Review</span>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
